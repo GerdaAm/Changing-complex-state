fdc36b0f8e5185180eb60f8bc258fae4111c9657
GerdaAm <63974776+GerdaAm@users.noreply.github.com>
2021-01-19 06:57:15 +0200

    Initial Commit Keep state for both input fields

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [fullName, setFullName] = useState({ fName: "", lName: "" });
+
+  function updateFullName(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+
+    setFullName((prevValue) => {
+      if (inputName === "fName") {
+        return { fName: newValue, lName: prevValue.lName };
+      } else if (inputName === "lName") {
+        return { fName: prevValue.fName, lName: newValue };
+      }
+    });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {fullName.fName} {fullName.lName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
+        <input
+          name="fName"
+          placeholder="First Name"
+          type="text"
+          onChange={updateFullName}
+          value={fullName.fName}
+        />
+        <input
+          name="lName"
+          placeholder="Last Name"
+          type="text"
+          onChange={updateFullName}
+          value={fullName.lName}
+        />
         <button>Submit</button>
       </form>
     </div>
